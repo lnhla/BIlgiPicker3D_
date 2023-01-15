383250b82c98c40f18b610b475a57039bd09defd
leen <lnanahla@gmail.com>
2023-01-15 19:13:17 +0300

    Reuploaded entire project due to errors with sourcetree & github; Game completed & fixed; Level  2 (minigame) added and completed

---
+++ Assets/Scripts/Signals/InputSignals.cs
@@ -0,0 +1,17 @@
+using Extensions;
+using Keys;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class InputSignals : MonoSingleton<InputSignals>
+    {
+        public UnityAction onEnableInput = delegate { };
+        public UnityAction onDisableInput = delegate { };
+        public UnityAction onFirstTimeTouchTaken = delegate { };
+        public UnityAction onInputTaken = delegate { };
+        public UnityAction onInputReleased = delegate { };
+        public UnityAction<HorizontalnputParams> onInputDragged = delegate { };
+        public UnityAction onMiniGameValues = delegate { };
+    }
+}
